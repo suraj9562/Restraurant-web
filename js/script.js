1d5ed0da81420c07dc121da3e8bdbbd7194377a3
suraj9562 <wadikarsuraj02@gmail.com>
2022-01-29 00:03:30 +0530

    Done with navbar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let navbar = document.querySelector(".header .navbar");
+let menuBtn = document.querySelector("#menu-btn");
+
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
