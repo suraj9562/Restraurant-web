de308759fcd700c7e973280d0a8f7759769965b1
suraj9562 <wadikarsuraj02@gmail.com>
2022-01-31 21:52:21 +0530

    done with food-order preview

--- js/script.js
+++ js/script.js
@@ -37,3 +37,26 @@ var swiper = new Swiper(".food-slider", {
     },
   },
 });
+
+let previewContainer = document.querySelector(".food-preview-container");
+let previewBox = previewContainer.querySelectorAll(".food-preview");
+
+document.querySelectorAll(".food .slide").forEach((food) => {
+  food.onclick = () => {
+    previewContainer.style.display = "flex";
+    let name = food.getAttribute("data-name");
+    previewBox.forEach((preveiw) => {
+      let target = preveiw.getAttribute("data-target");
+      if (name == target) {
+        preveiw.classList.add("active");
+      }
+    });
+  };
+});
+
+previewContainer.querySelector("#close-preview").onclick = () => {
+  previewContainer.style.display = "none";
+  previewBox.forEach((close) => {
+    close.classList.remove("active");
+  });
+};
